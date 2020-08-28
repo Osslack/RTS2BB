543ba91db3e80a5e86834c35f25990eedf6bd23e
osslack <simonoswaldvhm@gmail.com>
2020-08-28 09:02:13 +0200

    Allow passing options via CLI

--- index.js
+++ index.js
@@ -305,4 +305,32 @@ function format (input, options = {}) {
   }
 }
 
-console.log(format(fs.readFileSync(process.argv[2], { encoding: 'utf8' }), { ignore_intended_reps: false }));
+function parseCLIOptions(argv){
+  const options = {};
+  for(let i = 3; i < process.argv.length; i++){
+    if(process.argv[i] === "--help") {
+      console.log(
+                  'node index.js <file> [option]+ [--help]\n\n' + 
+                  'Available options are:\n'
+                  +'- underlined_exercise\n'
+                  +'- bold_exercise\n'
+                  +'- italic_exercise\n'
+                  +'- underlined_notes\n'
+                  +'- bold_notes\n'
+                  +'- italic_notes\n'
+                  +'- ignore_intended_reps\n'
+      );
+      process.exit(0);
+    }
+    options[process.argv[i]] = true;
+  }
+
+  return options;
+}
+
+if(process.argv.length == 2){
+  console.log('File argument is missing.');
+  process.exit(1);
+}
+
+console.log(format(fs.readFileSync(process.argv[2], { encoding: 'utf8' }), parseCLIOptions(process.argv)));
